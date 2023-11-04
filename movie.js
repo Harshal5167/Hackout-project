c56f30a85b9b0a15b93018313c5c453c07bd916c
Harshal Gainer <harshalgainer@gmail.com>
2023-11-04 12:37:24 +0530

    new Changes

--- movie.js
+++ movie.js
@@ -6,10 +6,15 @@ const searchURL =
     "https://api.themoviedb.org/3/search/movie?&api_key=19f84e11932abbc79e6d83f82d6d1045&query="
 const form = document.getElementById("search-form")
 const query = document.getElementById("query")
-const root = document.getElementById("root")
+const root2 = document.getElementById("root2")
 let movies = [],
     page = 1,
     inSearchPage = false
+const cardMedia=document.getElementById("card-media")
+
+const cardMediaOnClick=()=>{
+    console.log("hello")
+}
 
 async function fetchData(URL) {
     try {
@@ -34,9 +39,9 @@ const getSpecificPage = (page) => {
 const movieCard = (movie) =>
     `<div class="col">
           <div class="card">
-            <a class="card-media" href="">
+            <div class="card-media" id="card-media" onClick="cardMediaOnClick()">
               <img src="${movie.poster_path}" alt="PUBG Mobile" width="100%" />
-            </a>
+            </div>
             <div class="card-content">
               <div class="card-cont-header">
                 <div class="cont-left">
@@ -49,7 +54,7 @@ const movieCard = (movie) =>
         </div>`
 
 const showResults = (items) => {
-    let content = !inSearchPage ? root.innerHTML : ""
+    let content = !inSearchPage ? root2.innerHTML : ""
     if (items && items.length > 0) {
         items.map((item) => {
             let { poster_path, original_title, release_date, overview } = item
@@ -81,7 +86,7 @@ const showResults = (items) => {
         content += "<p>Something went wrong!</p>"
     }
 
-    root.innerHTML = content // Inject content to root
+    root2.innerHTML = content 
 }
 
 const handleLoadMore = () => {
